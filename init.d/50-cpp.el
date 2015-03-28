7dd76347ba527775f8042fec645ce545b3c65fcc
Kenichi HIROSE <seroron@outlook.com>
2015-03-28 22:42:47 +0900

    modify cpp mode

--- init.d/50-cpp.el
+++ init.d/50-cpp.el
@@ -2,3 +2,14 @@
 (setq auto-mode-alist
       (append '(("\\.h$" . c++-mode))
               auto-mode-alist))
+
+(defun my-c-c++-mode-init ()
+  (c-set-style "bsd")
+  (setq c-basic-offset 4)
+  (setq indent-tabs-mode nil)
+  (setq c-hungry-delete-key t)
+  )
+
+(add-hook 'c-mode-hook 'my-c-c++-mode-init)
+(add-hook 'c++-mode-hook 'my-c-c++-mode-init)
+
