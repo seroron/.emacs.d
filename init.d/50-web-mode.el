09546659de3bb9cef6c24dc2901d83618a61737c
Kenichi HIROSE <seroron@outlook.com>
2015-10-31 22:10:14 +0900

    web-mode indent size

--- init.d/50-web-mode.el
+++ init.d/50-web-mode.el
@@ -1,3 +1,8 @@
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
+
+(defun web-mode-hook ()
+  "web mode hook"
+  (setq web-mode-markup-indent-offset 2))
+(add-hook 'web-mode-hook 'web-mode-hook)
