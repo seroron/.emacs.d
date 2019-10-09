d1c088261625960dec2e060fb44b0ab94ad6fe3e
Kenichi HIROSE <seroron@outlook.com>
2019-10-09 23:11:49 +0900

    ag rg switch

--- init.d/20-helm-ag-mode.el
+++ init.d/20-helm-ag-mode.el
@@ -2,7 +2,10 @@
 (require 'helm-files)
 (require 'helm-ag)
 
-(setq helm-ag-base-command "rg --no-heading --ignore-case")
+(if (executable-find "rg")
+    (setq helm-ag-base-command "rg --no-heading --ignore-case") ; ripgrep
+    (setq helm-ag-base-command "ag --nocolor --nogroup --ignore-case") ; The Silver Searcher
+)
 
 (define-key global-map (kbd "M-s") 'helm-do-ag-project-root)
 (define-key global-map (kbd "C-M-s") 'helm-ag-this-file)
