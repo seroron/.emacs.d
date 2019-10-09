3342405ce93c2db8c6cddbad18b63294e20d192c
Kenichi HIROSE <seroron@outlook.com>
2019-10-09 23:11:03 +0900

    compliation color

---
+++ init.d/10-compilation.el
@@ -0,0 +1,5 @@
+(require 'ansi-color)
+(defun my/ansi-colorize-buffer ()
+  (let ((buffer-read-only nil))
+    (ansi-color-apply-on-region (point-min) (point-max))))
+(add-hook 'compilation-filter-hook 'my/ansi-colorize-buffer)
