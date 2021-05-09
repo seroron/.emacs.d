cf6d1277545f5c308314c64f6ef549315fbc1abf
Kenichi HIROSE <seroron@outlook.com>
2021-05-09 17:09:18 +0900

    doom-tomorrow-night

--- init.el
+++ init.el
@@ -115,12 +115,12 @@
 (setq ring-bell-function 'ignore)
 
 ;; Theme
-(load-theme 'wombat t)
+;(load-theme 'wombat t)
 
-;; (leaf doom-themes
-;;   :ensure t
-;;   :config
-;;   (load-theme 'doom-vibrant t))
+(leaf doom-themes
+  :ensure t
+  :config
+  (load-theme 'doom-tomorrow-night t))
 
 (leaf doom-modeline
   :ensure t
@@ -154,29 +154,29 @@
   :ensure t
   :hook
   (prog-mode-hook . rainbow-delimiters-mode)
-  :config
-  (set-face-attribute 'rainbow-delimiters-depth-1-face nil
-                      :foreground "#78c5d6")
-  (set-face-attribute 'rainbow-delimiters-depth-2-face nil
-                      :foreground "#bf62a6")
-  (set-face-attribute 'rainbow-delimiters-depth-3-face nil
-                      :foreground "#459ba8")
-  (set-face-attribute 'rainbow-delimiters-depth-4-face nil
-                      :foreground "#e868a2")
-  (set-face-attribute 'rainbow-delimiters-depth-5-face nil
-                      :foreground "#79c267")
-  (set-face-attribute 'rainbow-delimiters-depth-6-face nil
-                      :foreground "#f28c33")
-  (set-face-attribute 'rainbow-delimiters-depth-7-face nil
-                      :foreground "#c5d647")
-  (set-face-attribute 'rainbow-delimiters-depth-8-face nil
-                      :foreground "#f5d63d")
-  (set-face-attribute 'rainbow-delimiters-depth-9-face nil
-                      :foreground "#78c5d6")
-  (set-face-attribute 'rainbow-delimiters-unmatched-face nil
-                      :foreground 'unspecified
-                      :inherit 'show-paren-mismatch
-                      :strike-through t)
+  ;; :config
+  ;; (set-face-attribute 'rainbow-delimiters-depth-1-face nil
+  ;;                     :foreground "#78c5d6")
+  ;; (set-face-attribute 'rainbow-delimiters-depth-2-face nil
+  ;;                     :foreground "#bf62a6")
+  ;; (set-face-attribute 'rainbow-delimiters-depth-3-face nil
+  ;;                     :foreground "#459ba8")
+  ;; (set-face-attribute 'rainbow-delimiters-depth-4-face nil
+  ;;                     :foreground "#e868a2")
+  ;; (set-face-attribute 'rainbow-delimiters-depth-5-face nil
+  ;;                     :foreground "#79c267")
+  ;; (set-face-attribute 'rainbow-delimiters-depth-6-face nil
+  ;;                     :foreground "#f28c33")
+  ;; (set-face-attribute 'rainbow-delimiters-depth-7-face nil
+  ;;                     :foreground "#c5d647")
+  ;; (set-face-attribute 'rainbow-delimiters-depth-8-face nil
+  ;;                     :foreground "#f5d63d")
+  ;; (set-face-attribute 'rainbow-delimiters-depth-9-face nil
+  ;;                     :foreground "#78c5d6")
+  ;; (set-face-attribute 'rainbow-delimiters-unmatched-face nil
+  ;;                     :foreground 'unspecified
+  ;;                     :inherit 'show-paren-mismatch
+  ;;                     :strike-through t)
   )
 
 
